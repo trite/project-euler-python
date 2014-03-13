4019127b83ef07a8be71d3f377af869974b4af34
pbrunner <pbrunner@godaddy.com>
2014-03-12 23:29:12 -0700

    Updated problem 1 with answer.

--- TriteEulerPython/Problem001.py
+++ TriteEulerPython/Problem001.py
@@ -4,4 +4,6 @@
 # Find the sum of all the multiples of 3 or 5 below 1000.
 the3 = set(range(3, 1000, 3))
 the5 = set(range(5, 1000, 5))
-print(sum(the3 | the5))
+print('Answer: ' + str(sum(the3 | the5)))
+
+# Answer: 233168
