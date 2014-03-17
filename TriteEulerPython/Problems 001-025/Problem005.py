3c742bfd0155d9e1903ec118b598149c05318ccd
pbrunner <pbrunner@godaddy.com>
2014-03-17 12:40:38 -0700

    Correct solution for problem 005

---
+++ TriteEulerPython/Problems 001-025/Problem005.py
@@ -0,0 +1,21 @@
+# 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+# 
+# What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+nMax = 20
+nSec = nMax - 1
+nStep = nMax * nSec
+current = nStep
+
+def isMultiple(testValue, fMax):
+    for x in range(fMax, 2, -1):
+        if testValue % x != 0:
+            return False
+    return True
+
+while(not(isMultiple(current, nMax))):
+    current += nStep
+
+print("Answer: " + str(current))
+
+# Answer: 232792560
