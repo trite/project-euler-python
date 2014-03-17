b03d5d6f97bd397739b2fb8ea0f122e275d640b6
pbrunner <pbrunner@godaddy.com>
2014-03-17 12:26:47 -0700

    Solved problems 003 and 004 correctly.

---
+++ TriteEulerPython/Problems 001-025/Problem003.py
@@ -0,0 +1,21 @@
+# The prime factors of 13195 are 5, 7, 13 and 29.
+#
+# What is the largest prime factor of the number 600851475143 ?
+
+primes, current, target = [2, 3, 5], 7, 600851475143
+sqrttar = int(target ** (1/2))
+while current <= sqrttar:
+    sqrcur = int(current ** (1/2))
+    found = False
+    for x in primes:
+        if (x > sqrcur):
+            break
+        if (current % x == 0):
+            found = True
+            break
+    if (found == False):
+        primes.append(current)
+    current += 2
+print('Answer: ' + str(max([x for x in primes if target % x == 0])))
+
+# Answer: 6857
---
+++ TriteEulerPython/Problems 001-025/Problem004.py
@@ -0,0 +1,19 @@
+# A palindromic number reads the same both ways. The largest palindrome made from
+# the product of two 2-digit numbers is 9009 = 91 x 99.
+# 
+# Find the largest palindrome made from the product of two 3-digit numbers.
+
+def isPalindrome(inputVal):
+    reversed = int((str(inputVal)[::-1]))
+    return reversed == inputVal
+
+nMax, nMin, highest = 999, 900, 0
+for x in range(nMax, nMin, -1):
+    for y in range(nMax, nMin, -1):
+        product = x * y
+        if ((product > highest) and isPalindrome(product)):
+            highest = product
+            
+print("Answer: " + str(highest))
+
+# Answer: 906609
