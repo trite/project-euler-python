6bed744e57e5647f506da78e13be0ac260943cb6
pbrunner <pbrunner@godaddy.com>
2014-03-25 14:31:33 -0700

    Problem 015 added/commented with problem, not started.

--- TriteEulerPython/Problems 001-025/Problem015.py
+++ TriteEulerPython/Problems 001-025/Problem015.py
@@ -1,1 +1,12 @@
 # TODO
+
+print('''
+Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down,
+there are exactly 6 routes to the bottom right corner.
+
+***Picture is here on the actual site***
+
+How many such routes are there through a 20×20 grid?
+
+
+''')
